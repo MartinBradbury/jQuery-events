b8cf638619291f78a8f3820fe188ee0321b2a1ac
Martin Bradbury <martinbradbury@btinternet.com>
2024-01-26 13:07:25 +0000

    some changes

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
-    $("#stream1_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream1").addClass('highlight_stream');
+//     $("#stream1_btn").on("click", function() rewritten code below.
+    $("#stream1_btn").mouseenter(function() {
+     //    $(".stream1").removeClass('highlight_stream');
+     //    $(".stream2").removeClass('highlight_stream');
+     //    $(".stream3").removeClass('highlight_stream');
+     //    $(".stream1").addClass('highlight_stream');
+        $(".stream1").fadeTo(1000, 0.1);
    });
+
+   $("#stream1_btn").mouseleave(function() {
+        $(".stream1").fadeTo(1000, 1);
+   });
+
+
    $("#stream2_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
@@ -18,3 +26,4 @@ $(document).ready(function() {
         $(".stream3").addClass('highlight_stream');
    });
 }); 
+
